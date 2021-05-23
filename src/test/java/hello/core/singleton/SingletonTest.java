712378b7447ae69c18d2f9bb48f4a940bf3464a3
hye-rim <hrim39@gmail.com>
2021-05-23 21:18:16 +0900

    싱글톤 패턴을 구현해보고 싱글톤패턴의 장점을 눈으로 확인하기

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SingletonTest {
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
@@ -21,10 +23,22 @@ public class SingletonTest {
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
 
-        Assertions.assertNotEquals(memberService1,memberService2);
+        assertNotSame(memberService1,memberService2);
 
         // 스프링 없는 순수한 DI 컨테이너인 AppConfig는 요청을 할 때마다 객체를 새로 생성
         // 고객 트래픽이 초당 100이 나오면 초당 100개(이상의) 객체가 생성/소멸 -> 메모리낭비가 심함
         // 해결 방안은 해당 객체를 한개만 생성하고 공유하도록 설계한다 ! -> 즉 싱글톤패턴 !!
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertSame(singletonService1,singletonService2);
     }
 }
